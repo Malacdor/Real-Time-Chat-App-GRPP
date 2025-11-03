769c4695f8f72d31ffd57ca8402d1cebb8409629
Diego Salas <142828587+Malacdor@users.noreply.github.com>
2025-11-02 18:42:07 -0700

    Initialize Express server with static file serving

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const PORT = process.env.PORT || 4000
+app.use(express.static(path.join(__dirname, 'public')))
+const server = app.listen(PORT, () => {
+  console.log(`chat server on port ${PORT}`)
+})
