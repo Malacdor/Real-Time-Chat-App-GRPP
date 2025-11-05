e44941db75562920a1b107e198f30b13eee42653
Diego Salas <142828587+Malacdor@users.noreply.github.com>
2025-11-04 21:46:52 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const PORT = process.env.PORT || 4000
-app.use(express.static(path.join(__dirname, 'public')))
+const PORT = process.env.PORT || 4000 // server on port 4000  
+app.use(express.static(path.join(__dirname, 'public'))) // index.html being public means we need no route
 const server = app.listen(PORT, () => {
   console.log(`chat server on port ${PORT}`)
 })
