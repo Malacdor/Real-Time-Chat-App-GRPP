791c756715ed1300262a495ba77659b3f43fc064
Diego Salas <142828587+Malacdor@users.noreply.github.com>
2025-11-25 22:58:34 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const PORT = process.env.PORT || 4000 // server on port 4000  
-app.use(express.static(path.join(__dirname, 'public'))) // index.html being public means we need no route
-const server = app.listen(PORT, () => {
-  console.log(`chat server on port ${PORT}`)
-})
+const PORT = process.env.PORT || 4000
+const server = app.listen(PORT, () => console.log(`💬 server on port ${PORT}`))
+
+const io = require('socket.io')(server)
+
+app.use(express.static(path.join(__dirname, 'public')))
+let socketsConected = new Set()
+io.on('connection', onConnected)
+
+function onConnected(socket) {
+  console.log('Socket connected', socket.id)
+  socketsConected.add(socket.id)
+  io.emit('clients-total', socketsConected.size)
+
+  socket.on('disconnect', () => {
+    console.log('Socket disconnected', socket.id)
+    socketsConected.delete(socket.id)
+    io.emit('clients-total', socketsConected.size)
+  })
+
+  socket.on('message', (data) => {
+    // console.log(data)
+    socket.broadcast.emit('chat-message', data)
+  })
+
+  socket.on('feedback', (data) => {
+    socket.broadcast.emit('feedback', data)
+  })
+}
