c7360cbdfe07a0c03b38920bbfb346f867b0857a
Diego Salas <142828587+Malacdor@users.noreply.github.com>
2025-11-25 23:02:45 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 4000
+
+// Start the HTTP server instance
+// Note: Socket.io will attach to this server, so it must be created separately
 const server = app.listen(PORT, () => console.log(`💬 server on port ${PORT}`))
 
+
+// Initialize Socket.io using the existing HTTP server
+// This below enables the real time communication with users
 const io = require('socket.io')(server)
 
+// Serve static assets (HTML, CSS, JS) from the /public directory
+// Important! This lets the front end to interact with the WebSocket backend stuff
 app.use(express.static(path.join(__dirname, 'public')))
 let socketsConected = new Set()
 io.on('connection', onConnected)
